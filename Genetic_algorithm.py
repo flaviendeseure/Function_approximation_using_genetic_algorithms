bcead223d3ee94291581459d92f77e3a8919d3b9
Flavien DESEURE--CHARRON <flavien.deseure@gmail.com>
2021-12-04 14:20:07 +0100

    Update comments

--- Genetic_algorithm.py
+++ Genetic_algorithm.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Function approximation using genetic algorithm
-
-# %% ## I- Initialisation
+# I- Initialization
 # Basics import
 import numpy as np
 import random
@@ -11,196 +10,436 @@ import pandas as pd
 # Performance
 import time
 
-#%%
+# Upload the data
 def upload(name):
-  temp = pd.read_csv(name,sep=";")
-  temp.rename(columns={"#i": "i"}, inplace=True)
-  return temp
+    """
+    Upload the data
 
-temp = upload("C:\\Users\\flavi\\OneDrive\\Documents\\ESILV\\A3\\S2\\Datascience et IA\\TD\\Rendu algorithme Génétique\\temperature_sample.csv")
-temp.head(3)
+    Parameters
+    ----------
+    name: str
+          file name (or path)
+    Returns
+    -------
+    temp: pandas.core.frame.DataFrame
+          the temperature dataframe
+    """
+    temp = pd.read_csv(name,sep=";")
+    temp.rename(columns={"#i": "i"}, inplace=True)
+    return temp
 
-# %% ## II- Méthodes
+temp = upload(r"data/temperature_sample.csv")
 
-### Fonctions d'aide
+# II- Class and Methods
+# Help functions
 
 def convert2binary(nb, max = 20):
-  nb = bin(nb).split("b")[1]
-  while len(nb) < len(bin(max).split("b")[1]):
-    nb = "0" + nb
-  return nb
+    """
+    Convert a number to binary
+
+    Parameters
+    ----------
+    nb: int
+        the number to convert
+    max: int
+        the max number of bits
+    Returns
+    -------
+    binary: str
+            the binary number
+    """
+    nb = bin(nb).split("b")[1]
+    while len(nb) < len(bin(max).split("b")[1]):
+      nb = "0" + nb
+    return nb
 
 def t(i,a,b,c):
-  return [np.sum([np.cos((np.pi)*k*(b**n))*(float(a)**n) for n in range(c)]) for k in i]
+    """
+    Function to calculate the temperature
+
+    Parameters
+    ----------
+    i: int
+      the time
+    a: float
+      the a coefficient
+    b: float
+      the b coefficient
+    c: float
+      the c coefficient
+    Returns
+    -------
+    t: float
+        the temperature
+    """
+    return [np.sum([np.cos((np.pi)*k*(b**n))*(float(a)**n) for n in range(c)]) for k in i]
 
 def accuracy_method(a,b,c):
-  return sum(np.abs(t(temp["i"],a,b,c) - temp["t"]))/sum(np.abs(temp["i"]))*100
+    """
+    Calculate the accuracy of the method
 
-"""### Individu"""
+    Parameters
+    ----------
+    a: float
+        the a coefficient
+    b: float
+        the b coefficient
+    c: float
+        the c coefficient
+    Returns
+    -------
+    accuracy: float 
+              the accuracy
+    """
+    return sum(np.abs(t(temp["i"],a,b,c) - temp["t"]))/sum(np.abs(temp["i"]))*100
 
-class individu:
-  def __init__(self, a=None, b=None, c=None, bin=None): # Val => liste contenant a,b,c
-    self.bin = None
+# Individual class
+class individual:
+    """
+    Class to create an individual
+    """
+    def __init__(self, a=None, b=None, c=None, bin=None):
+        """
+        Initialize the individual
 
-    if a != None and b != None and c != None:
-      self.a, self.b, self.c = a, b, c
-    elif a != None and bin != None and len(bin) == 10:
-      self.a, self.bin = a, bin
-      self.b, self.c = int(bin[:5],2), int(bin[5:],2)
-    else:
-      self.a, self.b, self.c = random.uniform(0,1), random.randint(1, 21), random.randint(1, 21)      
+        Parameters
+        ----------
+        a: float
+            the a coefficient (default = None) if a is None, a random value is chosen
+        b: float
+            the b coefficient (default = None) if b is None, a random value is chosen
+        c: float
+            the c coefficient (default = None) if c is None, a random value is chosen
+        bin: str
+            the binary number (if you want to initialize the individual with a binary number)
+        """
+        self.bin = None
 
-    if self.bin == None:
-      self.bin = convert2binary(self.b) + convert2binary(self.c)
-    self.accuracy = self.fitness()
-      
-  def __str__(self):
-    return f"[{self.a} {self.b} {self.c}]"
+        if a != None and b != None and c != None:
+            self.a, self.b, self.c = a, b, c
+        elif a != None and bin != None and len(bin) == 10:
+            # Convert the binary number to decimal
+            self.a, self.bin = a, bin
+            self.b, self.c = int(bin[:5],2), int(bin[5:],2)
+        else:
+            self.a, self.b, self.c = random.uniform(0,1), random.randint(1, 21), random.randint(1, 21)      
 
-  def __eq__(self, autreIndividu):
-    return self.a == autreIndividu.a and self.b == autreIndividu.b and self.c == autreIndividu.c
-  
-  def fitness(self):
-    self.accuracy = np.linalg.norm(t(np.array(temp["i"]), self.a, self.b, self.c) - temp["t"])
-    return self.accuracy
+        if self.bin == None:
+            # Convert the decimal number to binary
+            self.bin = convert2binary(self.b) + convert2binary(self.c)
+        self.accuracy = self.fitness()
+        
+    def __str__(self):
+        """
+        Print the individual
+        """
+        return f"[{self.a} {self.b} {self.c}]"
 
-  def display_individu(self):
-    plt.figure(figsize=(10, 5))
-    i = np.arange(0, 4.7, 0.0005)
-    plt.grid()
-    plt.plot(i, t(i,self.a, self.b, self.c), c="b", linewidth=0.5)
-    plt.scatter(temp["i"], temp["t"], c='r',s=10)
+    def __eq__(self, autreIndividu):
+        """
+        Compare two individuals
+        """
+        return self.a == autreIndividu.a and self.b == autreIndividu.b and self.c == autreIndividu.c
+    
+    def fitness(self):
+        """
+        Calculate the fitness of the individual: mean squared error
 
-"""### Sélection, mutations et croisements"""
+        Returns
+        -------
+        fitness: float
+                  the fitness value
+        """
+        self.accuracy = np.linalg.norm(t(np.array(temp["i"]), self.a, self.b, self.c) - temp["t"])
+        return self.accuracy
 
-def mutation_binaire(parent,nb=4):
-  pm = random.uniform(0,1)
-  um = random.uniform(0,1)
-  if um < pm:
-    rank = []
-    while len(rank) < nb:
-      val = random.randint(0,len(parent))
-      while val in rank:
-        val = np.random.randint(0,len(parent))
-      rank.append(val)
-    parent_modified = ""
-    for i in range(len(parent)):
-      if i in rank:
-        parent_modified += "0" if parent[i] == "1" else "1"
-      else:
-        parent_modified += parent[i]
-    return parent_modified
-  return parent
+    def display_individu(self):
+        """
+        Display the individual
+        """
+        plt.figure(figsize=(10, 5))
+        i = np.arange(0, 4.7, 0.0005)
+        plt.grid()
+        plt.plot(i, t(i,self.a, self.b, self.c), c="b", linewidth=0.5)
+        plt.scatter(temp["i"], temp["t"], c='r',s=10)
 
-def mutation_reel(parent):
-  mutation_prob=random.randint(0,10)
-  if mutation_prob < 3:
-    vary = 1 + random.uniform(-0.05, 0.05)
-    rem = parent*vary
-    if abs(rem) <= 10:
-      parent = rem
-  return parent
+# Selection, Mutations et Crossovers
+def binary_mutation(parent,nb=4):
+    """
+    Mutate the binary number of the individual
 
-def croisement_binaire(parent1,parent2):
-  p1 = random.randint(0, 10)
-  p2 = random.randint(0, 10)
-  while p2 == p1:
+    Parameters
+    ----------
+    parent: individual
+            the individual to mutate
+    nb: int
+        the number of bits to mutate
+    Returns
+    -------
+    child: individual
+           the mutated individual
+    """
+    pm = random.uniform(0,1)
+    um = random.uniform(0,1)
+    if um < pm:
+        rank = []
+        while len(rank) < nb:
+            val = random.randint(0,len(parent))
+            while val in rank:
+                val = np.random.randint(0,len(parent))
+            rank.append(val)
+        parent_modified = ""
+        for i in range(len(parent)):
+            if i in rank:
+                parent_modified += "0" if parent[i] == "1" else "1"
+            else:
+                parent_modified += parent[i]
+        return parent_modified
+    return parent
+
+def real_mutation(parent):
+    """
+    Mutate the real number of the individual
+
+    Parameters
+    ----------
+    parent: individual
+            the individual to mutate  
+    Returns
+    ------- 
+    child: individual
+            the mutated individual
+    """
+    mutation_prob=random.randint(0,10)
+    if mutation_prob < 3:
+        vary = 1 + random.uniform(-0.05, 0.05)
+        rem = parent*vary
+        if abs(rem) <= 10:
+            parent = rem
+    return parent
+
+def binary_crossover(parent1,parent2):
+    """
+    Crossover the binary number of the individuals
+    
+    Parameters
+    ----------
+    parent1: individual
+              the first individual to crossover
+    parent2: individual
+              the second individual to crossover
+    Returns
+    -------
+    child1: individual  
+            the first child of the crossover
+    child2: individual
+            the second child of the crossover
+    """
+    p1 = random.randint(0, 10)
     p2 = random.randint(0, 10)
-  p1,p2 = min(p1,p2),max(p1,p2)
-  enfant1 = parent1[0:p1]+parent2[p1:p2+1]+parent1[p2+1:10]
-  enfant2 = parent2[0:p1]+parent1[p1:p2+1]+parent2[p2+1:10]
-  return enfant1, enfant2
+    while p2 == p1:
+        p2 = random.randint(0, 10)
+    p1,p2 = min(p1,p2),max(p1,p2)
+    child1 = parent1[0:p1]+parent2[p1:p2+1]+parent1[p2+1:10]
+    child2 = parent2[0:p1]+parent1[p1:p2+1]+parent2[p2+1:10]
+    return child1, child2
 
-def croisement_real(parent1,parent2,eta=2): #eta entre 2 et 5: (2 => plus de variations entre les parents, 5 => moins de variations)
+def real_crossover(parent1,parent2,eta=2): 
+  """
+  Crossover between two real numbers based on simulated binary crossover (SBX)
+  eta = 2 => more variation between parents
+  eta = 5 => less variation between parents
+
+  Parameters 
+  ----------
+  parent1: float
+           the first parent
+  parent2: float
+           the second parent
+  eta: float
+       the crossover rate (default = 2)
+       Returns
+       -------
+       child1: float
+       """
   u = random.uniform(0,1)
   beta = (2*u)**(1/(eta+1)) if u<=0.5 else (1/(2*(1-u)))**(1/(eta+1))
-  enfant1 = 0.5*((1+beta)*parent1 + (1-beta)*parent2)
-  enfant2 = 0.5*((1-beta)*parent1 + (1+beta)*parent2)
-  return enfant1, enfant2
+  child1 = 0.5*((1+beta)*parent1 + (1-beta)*parent2)
+  child2 = 0.5*((1-beta)*parent1 + (1+beta)*parent2)
+  return child1, child2
 
-def generer_pop(count):
-  return [individu() for i in range(count)]
+def generate_pop(count):
+    """
+    Generate a population of individuals
+    
+    Parameters
+    ----------
+    count: int
+           the number of individuals to generate
+    Returns
+    -------
+    population: list
+                the generated population 
+    """
+    return [individual() for i in range(count)]
 
 def evaluate(pop):
-  return sorted(pop, key=individu.fitness)
+    """ 
+    Evaluate the population with the fitness function
+
+    Parameters
+    ----------
+    pop: list
+          the population to evaluate
+    Returns
+    -------
+    pop: list 
+        the evaluated population sorted by fitness value
+    """
+    return sorted(pop, key=individual.fitness)
 
 def selection(pop, hcount, lcount):
-  return pop[:hcount] + pop[-lcount:]
+    """
+    Select the best and worst individuals from the population
+    In fact, elitism is not the best way to do selection.
+    I combined the best and worst individuals to create a new population and avoid the elitism.
 
-def croisement(parent1, parent2):
-  e1_a, e2_a = croisement_real(parent1.a, parent2.a)  
-  e1_binaire, e2_binaire = croisement_binaire(parent1.bin, parent2.bin)
-  return [individu(a = e1_a, bin = e1_binaire), individu(a = e2_a, bin = e2_binaire)]
+    Parameters
+    ----------
+    pop: list
+          the population to select from
+    hcount: int
+            the number of best individuals to select
+    lcount: int
+            the number of worst individuals to select
+    Returns
+    -------
+    pop: list
+        the selected population
+    """
+    return pop[:hcount] + pop[-lcount:]
+
+def crossover(parent1, parent2):
+    """
+    Crossover between two individuals
+    Combined the crossover of binary and real numbers
+
+    Parameters
+    ----------
+    parent1: individual
+              the first parent
+    parent2: individual 
+              the second parent
+    Returns
+    -------
+    children: list
+                the list of children
+    """
+    e1_a, e2_a = real_crossover(parent1.a, parent2.a)  
+    e1_binaire, e2_binaire = binary_crossover(parent1.bin, parent2.bin)
+    children = [individual(a = e1_a, bin = e1_binaire), individual(a = e2_a, bin = e2_binaire)]
+    return children
 
 def mutation(parent):
-  return individu(a = mutation_reel(parent.a), bin = mutation_binaire(parent.bin))
+    """
+    Mutate the individual
+    Combined the mutation of binary and real numbers
 
-# %% ## III- Algorithme
+    Parameters
+    ----------
+    parent: individual
+            the individual to mutate
+    Returns
+    -------
+    child: individual
+           the mutated individual
+    """
+    child = individual(a = real_mutation(parent.a), bin = binary_mutation(parent.bin))
+    return child
 
-def algoloopSimple(SEUIL, TAILLE_POP, REPARTITION=[10/30,4/30]):
-  pop = generer_pop(TAILLE_POP)
-  nbriteration=0
-  history = []
-  solution_trouvee = False
-  while not solution_trouvee:
-    nbriteration+=1
+# III- Main loop
+def genetic_algorithm(threshold, pop_size, repartition=[10/30,4/30]):
+    """
+    The main algorithm of the genetic algorithm
+    Parameters
+    ----------
+    threshold: float
+                the threshold of fitness (MSE) value to reach
+    pop_size: int
+              the population size
+    repartition: list
+                  the repartition of the best and worst individuals
+    Returns
+    -------
+    best_individual: individual
+                      the best individual found
+    """
+    pop = generate_pop(pop_size)
+    nbriteration=0
+    history = []
+    solution_trouvee = False
+    while not solution_trouvee:
+        nbriteration+=1
+
+        evaluation = evaluate(pop)
+        history.append(evaluation[0].accuracy)
+
+        if evaluation[0].accuracy < threshold:
+            solution_trouvee = True
+
+        print("\riteration numéro :", nbriteration, "- Fitness:", evaluation[0].accuracy, end="")
+
+        select = selection(evaluation, int(repartition[0] * pop_size), int(repartition[1] * pop_size))
+        croises = []
+        for i in range(0, len(select)//2):
+            croises += crossover(random.choice(select),random.choice(select))
+        mutes = []
+        for i in range(len(select)):
+            mutes.append(mutation(select[i]))
+        newalea = generate_pop(pop_size - int(repartition[0]*pop_size) - int(repartition[1]*pop_size))
+
+        pop=select[:]+croises[:]+mutes[:]+newalea[:]
 
     evaluation = evaluate(pop)
-    history.append(evaluation[0].accuracy)
+    best_ind = evaluation[0]
+    return history, best_ind, nbriteration
 
-    if evaluation[0].accuracy < SEUIL:
-      solution_trouvee = True
+if __name__ == "__main__":
+    # IV- Results
+    debut = time.process_time()
+    history, best_ind, nb_iter= genetic_algorithm(0.1813,100)
+    diff = time.process_time()-debut
 
-    #print("iteration numéro :", nbriteration, "- Fitness:", evaluation[0].accuracy)
+    print(f"{round(diff,5)} seconds - {nb_iter} itérations")
+    print(best_ind.a, best_ind.b, best_ind.c, best_ind.accuracy)
+    print(accuracy_method(best_ind.a, best_ind.b, best_ind.c))
 
-    select = selection(evaluation, int(REPARTITION[0] * TAILLE_POP), int(REPARTITION[1] * TAILLE_POP))
-    croises = []
-    for i in range(0, len(select)//2):
-      croises += croisement(random.choice(select),random.choice(select))
-    mutes = []
-    for i in range(len(select)):
-      mutes.append(mutation(select[i]))
-    newalea = generer_pop(TAILLE_POP - int(REPARTITION[0]*TAILLE_POP) - int(REPARTITION[1]*TAILLE_POP))
+    plt.plot(history)
+    plt.show()
+    # V- Stats of the algorithm
+    val = []
+    for i in range(10):
+      debut = time.process_time()
+      history, best_ind, nb_iter= genetic_algorithm(0.1813,30)
+      diff = time.process_time()-debut
 
-    pop=select[:]+croises[:]+mutes[:]+newalea[:]
+      print(f"{round(diff,5)} seconds - {nb_iter} iterations")
+      print(best_ind.a, best_ind.b, best_ind.c, best_ind.accuracy)
+      print(accuracy_method(best_ind.a, best_ind.b, best_ind.c))
+      val.append(diff)
+    print(val)
+    print(np.mean(val))
+    plt.boxplot(val)
+    plt.title("Execution time") 
+    plt.show()
 
-  evaluation = evaluate(pop)
-  best_ind = evaluation[0]
-  return history, best_ind, nbriteration
+    # VI- Export the predictions
+    def generate_download_file(a,b,c):
+        """
+        Generate a csv file to download that contains the predictions
+        """
+        with open("flavien_deseure--charron.txt","w") as file:
+            file.write(f"{a};{b};{c}")
 
-# %% ## IV- Résultats
-
-debut = time.process_time()
-history, best_ind, nb_iter= algoloopSimple(0.1813,100)
-diff = time.process_time()-debut
-
-print(f"{round(diff,5)} seconds - {nb_iter} itérations")
-print(best_ind.a, best_ind.b, best_ind.c, best_ind.accuracy)
-print(accuracy_method(best_ind.a, best_ind.b, best_ind.c))
-
-plt.plot(history)
-
-# %%
-val = []
-for i in range(30):
-  debut = time.process_time()
-  history, best_ind, nb_iter= algoloopSimple(0.1813,30)
-  diff = time.process_time()-debut
-
-  print(f"{round(diff,5)} seconds - {nb_iter} itérations")
-  print(best_ind.a, best_ind.b, best_ind.c, best_ind.accuracy)
-  print(accuracy_method(best_ind.a, best_ind.b, best_ind.c))
-  val.append(diff)
-print(val)
-print(np.mean(val))
-plt.boxplot(val)
-
-
-
-# %% ## V- Téléchargement des données
-
-def generate_download_file(a,b,c):
-  with open("flavien_deseure--charron.txt","w") as file:
-    file.write(f"{a};{b};{c}")
-
-generate_download_file(best_ind.a, best_ind.b, best_ind.c)
+    generate_download_file(best_ind.a, best_ind.b, best_ind.c)
+    print("File generated")
